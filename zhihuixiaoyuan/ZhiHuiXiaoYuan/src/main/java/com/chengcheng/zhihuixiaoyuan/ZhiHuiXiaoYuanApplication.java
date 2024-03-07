f53e92fa2c759223946ac59446001115d69ed70e
yibai <404223879@qq.com>
2024-03-07 21:51:35 +0800

    1

---
+++ zhihuixiaoyuan/ZhiHuiXiaoYuan/src/main/java/com/chengcheng/zhihuixiaoyuan/ZhiHuiXiaoYuanApplication.java
@@ -0,0 +1,13 @@
+package com.chengcheng.zhihuixiaoyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiHuiXiaoYuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiHuiXiaoYuanApplication.class, args);
+    }
+
+}
---
+++ zhihuixiaoyuan/ZhiHuiXiaoYuan/src/test/java/com/chengcheng/zhihuixiaoyuan/ZhiHuiXiaoYuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengcheng.zhihuixiaoyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhiHuiXiaoYuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
