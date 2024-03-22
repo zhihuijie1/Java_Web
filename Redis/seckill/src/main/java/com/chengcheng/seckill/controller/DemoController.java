256f237e09a0e3aaae93ce3501c1f03f93e151b6
yibai <404223879@qq.com>
2024-03-22 16:58:56 +0800

    秒杀系统的项目配置

--- Redis/seckill/src/main/java/com/chengcheng/seckill/controller/DemoController.java
+++ Redis/seckill/src/main/java/com/chengcheng/seckill/controller/DemoController.java
@@ -1,5 +1,15 @@
 package com.chengcheng.seckill.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
 public class DemoController {
 
+    @GetMapping("/h")
+    public String getHello() {
+        return "hello";
+    }
 }
